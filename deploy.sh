4a4e463b2101dfb492504bbe285042cc670753b9
Sandro Pischinger <mail@sandropischinger.de>
2024-09-18 21:50:24 +0200

    clean dst dir in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-hugo && rsync -avz --delete public/ uber:~/html/
+hugo --cleanDestinationDir && rsync -avz --delete public/ uber:~/html/
 ssh uber << EOF
 chmod -R u=rwX,go=rX ~/html
 restorecon -R -v ~/html
