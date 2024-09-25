ae056d935ec6908731b8bc444fe2330c330c8b85
Sandro Pischinger <mail@sandropischinger.de>
2024-09-25 14:27:53 +0200

    rsync: exclude files

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-hugo --cleanDestinationDir && rsync -avz --delete public/ uber:~/html/
+hugo --cleanDestinationDir && rsync -avz --exclude=impressum.html --exclude=.htaccess --delete public/ uber:~/html/
 ssh uber << EOF
 chmod -R u=rwX,go=rX ~/html
 restorecon -R -v ~/html
