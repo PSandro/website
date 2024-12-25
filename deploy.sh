7a8e33a5dab2b17d55f928d0fb53c3e6b71e6c1c
Sandro Pischinger <mail@sandropischinger.de>
2024-12-25 23:31:24 +0100

    deploy script: keep cv dir

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-hugo --cleanDestinationDir && rsync -avz --exclude='*/raw/' --exclude='*/raw/*' --exclude=impressum.html --exclude=.htaccess --delete public/ uber:~/html/
+hugo --cleanDestinationDir && rsync -avz \
+  --exclude='/cv/' \
+  --exclude='/cv/*' \
+  --exclude='*/raw/' \
+  --exclude='*/raw/*' \
+  --exclude='impressum.html' \
+  --exclude='.htaccess' \
+  --delete public/ uber:~/html/
 ssh uber << EOF
 chmod -R u=rwX,go=rX ~/html
 restorecon -R -v ~/html
